32f91874461f6e809ec5eb0042bfaa864b74ace6
Vasil Valchev <96626766+VsVch@users.noreply.github.com>
2022-09-14 18:32:13 +0300

    Add project files.

---
+++ 01. Single Inheritance/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Farm
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog dog = new Dog();
+            dog.Bark();
+            dog.Bark();
+        }
+    }
+}
